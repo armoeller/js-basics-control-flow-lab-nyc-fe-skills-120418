ea6e9d410162cf6f075b1b81aba0153f557c0c9c
Andy Moeller <ar.moeller1@gmail.com>
2019-01-17 16:38:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ function scuberGreetingForFeet(someValue){
 
 
 function ternaryCheckCity(whatCity){
-  return whatCity ? 'Ok, sounds good.' : 'No go.';
+  return whatCity == 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
 // function ternaryCheckCity(Pittsburgh){
